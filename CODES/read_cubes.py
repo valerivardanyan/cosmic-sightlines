40dd658d8f475e4ee338035115a75c44349d5ce4
gmetin <metinatamail@gmail.com>
2021-07-12 21:47:25 +0900

    read

--- CODES/read_cubes.py
+++ CODES/read_cubes.py
@@ -1,11 +1,8 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 file_in = "/var/lib/libvirt/images/nbody/baorsd/run101/density_field/S020_CIC1024_for_xi.bin"
 
 
-nc = 1204
-file_0 = np.fromfile(file_in, dtype=np.single).reshape(nc,nc,nc)
-
-
-
+nc = 1024
+file_0 = np.fromfile(file_in, dtype=np.single)
 
